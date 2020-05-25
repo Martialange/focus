445da10ce3d2593f10db1567a5afef8ec4a764b8
Martialange <54242105+Martialange@users.noreply.github.com>
2020-05-25 23:54:39 +0100

    Create aprecu_Case.php

---
+++ Case/data/aprecu_Case.php
@@ -0,0 +1,29 @@
+<?php 
+$headings =[
+    'Profitability' => 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.',
+    'Market Size' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati velit harum architecto nam numquam illum iste, nesciunt nihil neque impedit consequatur molestias quidem iusto quis labore inventore, repellat enim adipisci.',
+    'Merger & Acquisition' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati velit harum architecto nam numquam illum iste, nesciunt nihil neque impedit consequatur molestias quidem iusto quis labore inventore, repellat enim adipisci.',
+
+];
+
+$marketing=[
+    'Welcome to the interactive Case Study '=>'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.'
+];
+$Matiere=[
+    'Profitability'=>'Profitability',
+    'Market_Size'=>'Market Size',
+    'Acquisition'=>'Merger & Acquisition'
+];
+
+$Profitability=[
+    ['/img/Case1.jpeg','Lorem ipsum dolor sa.',9,'Reding'],
+    ['/img/Case2.jpg','Lorem ipsum dolor si.',10,'Ending']
+];
+$Market_Size=[
+    ['/img/Case3.jpg','Lorem ipsum dolor.',12,'Attend'],
+    ['/img/Case4.jpg','Lorem ipsum dolor.',3,'Zerd']
+];
+$Acquisition=[
+        ['/img/Case5.png','Lorem ipsum d Officiis, at officia.',6,'Terao']
+]
+?>
